9b0d4b3b930d6ab1c774b3ab063a1cbed084f6cb
David Johan Hasiholan Parhusip <david.johan@ui.ac.id>
2023-02-06 20:59:14 +0700

    add blogs

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import mongoose from 'mongoose';
+import blogRouter from "./routes/blog-routes";
 import router from "./routes/user-routes";
 // import ro
 
 const app = express();
 app.use(express.json());
 app.use("/api/user", router) 
+app.use("/api/blog", blogRouter) 
 
 mongoose
 .connect(
---
+++ backend/controllers/blog-controller.js
@@ -0,0 +1,114 @@
+import mongoose from "mongoose";
+import Blog from "../model/Blog";
+import User from "../model/User";
+
+export const getAllBlogs = async(req, res, next) => {
+    let blogs;
+    try {
+        blogs = await Blog.find(); 
+    } catch(err) {
+        console.log(err);
+    }
+    if(!blogs) {
+        return res.status(404).json(
+            {
+                message: "No Blogs Found"
+            }
+        );
+    }
+    return res.status(200).json({blogs});
+}
+
+export const addBlog = async(req, res, next) => {
+    const {title, description, image, user} = req.body;
+    let existingUser;
+    try {
+        existingUser = await User.findById(user);
+    } catch(err) {
+        return console.log(err);
+    }
+    if (!existingUser) {
+        return res.status(400).json({message: "Unable find user by this id"});
+    }
+    const blog = new Blog({
+        title,
+        description,
+        image,
+        user
+    });
+    try {
+        const session = await mongoose.startSession();
+        session.startTransaction();
+        await blog.save({session});
+        existingUser.blogs.push(blog);
+        await existingUser.save({session});   
+        await session.commitTransaction();     
+    } catch(err) {
+        console.log(err);
+        return res.status(500).json({message: err});
+
+    }
+    return res.status(200).json({blog});
+}
+
+export const updateBlog = async(req, res, next) => {
+    const {title, description} = req.body;
+    const blogId = req.params.id;
+    let blog;
+    try {
+        blog = await Blog.findByIdAndUpdate(blogId, {
+            title,
+            description,
+        })
+    } catch(err) {
+        return console.log(err);
+    }
+    if (!blog) {
+        return res.status(500).json({message: "Unable to update blog!"});
+    }
+    return res.status(200).json({blog});
+}
+
+export const getById = async(req, res, next) => {
+    const blogId = req.params.id;
+    let blog;
+    try {
+        blog = await Blog.findById(blogId); 
+    } catch(err) {
+        console.log(err);
+    }
+    if(!blog) {
+        return res.status(404).json({message: "Blog not Found"});
+    }
+    return res.status(200).json({blog});
+}
+
+export const deleteBlog = async(req, res, next) => {
+    const blogId = req.params.id;
+    let blog;
+    try {
+        blog = await Blog.findByIdAndRemove(blogId).populate('user');
+        await blog.user.blogs.pull(blog);
+        await blog.user.save();
+    } catch(err) {
+        console.log(err);
+    }
+    if(!blog) {
+        return res.status(500).json({message: "Unable to delete"});
+    }
+    return res.status(200).json({message: "Success to delete"});
+}
+
+export const getByUserId = async(req, res, next) => {
+    const userId = req.params.id;
+    let userBlogs;
+    try {
+        userBlogs = await User.findById(userId).populate("blogs"); 
+    } catch(err) {
+        console.log(err);
+    }
+    if(!userBlogs) {
+        return res.status(404).json({message: "No blogs found"});
+    }
+    return res.status(200).json({userBlogs});
+}
